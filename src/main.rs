9b162127638319aa9785301372df01fc53554731
Ivor Wanders <ivor@iwanders.net>
2022-07-17 15:59:19 -0400

    Add cdc serial port, interact with LED through embedded_hal.

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,19 @@ use panic_halt as _;
 
 use nb::block;
 
+
+use cortex_m::asm::delay;
 use cortex_m_rt::entry;
 use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+
+
+// for serial.
+use stm32f1xx_hal::usb::{Peripheral, UsbBus};
+use usb_device::prelude::*;
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
+
+use embedded_hal::digital::v2::PinState::{Low, High};
+use embedded_hal::digital::v2::OutputPin;
 
 #[entry]
 fn main() -> ! {
@@ -30,7 +41,16 @@ fn main() -> ! {
 
     // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
     // `clocks`
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    // let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    // Set a real clock that allows usb.
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.MHz())
+        .sysclk(48.MHz())
+        .pclk1(24.MHz())
+        .freeze(&mut flash.acr);
+
+    assert!(clocks.usbclk_valid());
 
     // Acquire the GPIOC peripheral
     let mut gpioc = dp.GPIOC.split();
@@ -42,11 +62,78 @@ fn main() -> ! {
     let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
     timer.start(5.Hz()).unwrap();
 
+
+
+    // Setup usb serial
+
+    let mut gpioa = dp.GPIOA.split();
+
+    // BluePill board has a pull-up resistor on the D+ line.
+    // Pull the D+ pin down to send a RESET condition to the USB bus.
+    // This forced reset is needed only for development, without it host
+    // will not reset your device when you upload new firmware.
+    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+    usb_dp.set_low();
+    delay(clocks.sysclk().raw() / 100);
+
+    let usb = Peripheral {
+        usb: dp.USB,
+        pin_dm: gpioa.pa11,
+        pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
+    };
+    let usb_bus = UsbBus::new(usb);
+
+    let mut serial = SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(USB_CLASS_CDC)
+        .build();
+
+
+
     // Wait for the timer to trigger an update and change the state of the LED
+    let mut led_state = Low;
+    use embedded_hal::digital::v2::OutputPin;
     loop {
-        block!(timer.wait()).unwrap();
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        led.set_low();
+
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
+
+        let mut buf = [0u8; 64];
+
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+                // led.set_low(); // Turn on
+                led_state = if (led_state == Low) { High } else { Low };
+                embedded_hal::digital::v2::OutputPin::set_state(&mut led, led_state);
+
+                // Echo back in upper case
+                for c in buf[0..count].iter_mut() {
+                    if 0x61 <= *c && *c <= 0x7a {
+                        *c &= !0x20;
+                    }
+                }
+
+                let mut write_offset = 0;
+                while write_offset < count {
+                    match serial.write(&buf[write_offset..count]) {
+                        Ok(len) if len > 0 => {
+                            write_offset += len;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        // block!(timer.wait()).unwrap();
+        // led.set_high();
+        // block!(timer.wait()).unwrap();
+        // led.set_low();
     }
 }
